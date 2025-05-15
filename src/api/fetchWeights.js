720c91714d3d204b83c90e07b5e7207fe757beb9
Ajeong Shin <ajeong.shin95@gmail.com>
2025-05-15 23:31:18 +0200

    apply production env config

--- src/api/fetchWeights.js
+++ src/api/fetchWeights.js
@@ -1,7 +1,8 @@
 // 📁 src/api/fetchWeights.js
 export const fetchWeights = async (matrix) => {
-    const response = await fetch('http://127.0.0.1:5000/calculate', {
-      method: 'POST',
+  // http://127.0.0.1:5000/calculate: dev env
+  const response = await fetch('https://ahp-backend-725147247515.europe-west1.run.app/calculate', {
+    method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
